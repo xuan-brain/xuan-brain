e86d5b530bcf8a0224b9d8da92b0a7df017c1c0d
Dr.Guo <guoxiaozhong1990@outlook.com>
2026-01-28 23:46:08 +0800

    chore: update dependencies, gitignore and add paper_category migration

--- src-tauri/migration/src/lib.rs
+++ src-tauri/migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20220101_000001_create_table;
 mod m20260122_050159_create_categories;
 mod m20260123_161037_create_paper;
+mod m20260128_153129_create_paper_category;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20260122_050159_create_categories::Migration),
             Box::new(m20260123_161037_create_paper::Migration),
+            Box::new(m20260128_153129_create_paper_category::Migration),
         ]
     }
 }
---
+++ src-tauri/migration/src/m20260128_153129_create_paper_category.rs
@@ -0,0 +1,43 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        let db = manager.get_connection();
+        db.execute_unprepared(
+            "
+                -- 开启外键约束
+                PRAGMA foreign_keys = ON;
+
+                -- ==========================================
+                -- 1. 论文 分类 模块
+                -- ==========================================
+
+                CREATE TABLE paper_category (
+                    paper_id INTEGER,
+                    category_id INTEGER,
+                    PRIMARY KEY (paper_id, category_id),
+                    FOREIGN KEY (paper_id) REFERENCES papers(id) ON DELETE CASCADE,
+                    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
+                );
+
+                -- ==========================================
+                -- 2. 索引优化
+                -- ==========================================
+                CREATE INDEX idx_papers_category ON paper_category(category_id);
+            ",
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        Ok(())
+    }
+}
